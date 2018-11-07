53f435eadf038000a753d5b4839b445121d387a8
sevennt <zheng.leslie@qq.com>
2018-11-07 10:34:37 +0800

    remove istioctlExecutor && fix get Naftis namespace bug

--- src/api/handler/service.go
+++ src/api/handler/service.go
@@ -69,7 +69,7 @@ func Pods(c *gin.Context) {
 // Kubeinfo returns data like namespaces of Kubernetes.
 func Kubeinfo(c *gin.Context) {
 	var ns = service.ServiceInfo.Namespaces("").Exclude("kube-system", bootstrap.Args.IstioNamespace, bootstrap.Args.Namespace)
-	var retNs = make([]string, 0)
+	var retNs = make([]string, 0, len(ns))
 	for _, n := range ns {
 		retNs = append(retNs, n.Name)
 	}
