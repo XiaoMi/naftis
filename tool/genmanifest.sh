7a468a0406aab8facd097182f8eb535f9e55b768
sevennt <zheng.leslie@qq.com>
2018-11-23 10:27:19 +0800

    update naftis.yaml tags

--- tool/genmanifest.sh
+++ tool/genmanifest.sh
@@ -8,9 +8,10 @@ if [[ -z $1 ]]; then
     echo "choose your configuration file"
     exit 1
 fi
+TAG=`git describe --tags --abbrev=0` # TODO
 
 # generate naftis.yaml from naftis Charts.
-helm template install/helm/naftis --set api.image.repository=$HUB/naftis-api,ui.image.repository=$HUB/naftis-ui --set-file api.config=$1 --name naftis --namespace naftis > naftis.yaml
+helm template install/helm/naftis --set api.image.repository=$HUB/naftis-api,api.image.tag=$TAG,ui.image.repository=$HUB/naftis-ui,ui.image.tag=$TAG --set-file api.config=$1 --name naftis --namespace naftis > naftis.yaml
 
 # generate mysql.yaml from mysql Charts.
 helm template install/helm/mysql --set persistence.storageClass="manual",mysqlRootPassword="WlRncGh3UWY5VQ==",mysqlUser="naftis",mysqlPassword="naftisIsAwesome" --set-file initializationFiles."naftis\.sql"=tool/naftis.sql  --name naftis --namespace naftis >> mysql-cloud.yaml
